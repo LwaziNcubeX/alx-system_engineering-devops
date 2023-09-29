9e255df0499b4c98a6b6aa1c0dbe381dc3bbb3d6
LwaziNcubeX <lwazincubex@gmail.com>
2023-09-30 01:13:20 +0200

    added an infinity loop

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+int infinite_while(void);
+
+/**
+ * infinite_while - loop
+ *
+ * Return: void
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
 /**
  * main - creates 5 zombie processes
  *
@@ -18,8 +34,9 @@ int main(void)
 		if (pid == 0)
 		{
 			printf("Zombie process created, PID: %d\n", getpid());
-			exit(0);
+			exit (0);
 		}
 	}
+	infinite_while();
 	return (0);
 }
