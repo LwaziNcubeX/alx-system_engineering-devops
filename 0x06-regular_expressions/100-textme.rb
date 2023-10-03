3b8598a7ba94dc05a0d7c41a45c3b853bc43bc7a
LwaziNcubeX <lwazincubex@gmail.com>
2023-10-04 01:18:40 +0200

    updated script flags

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts "#{ARGV[0].match(/(?<=from:)\w*/)},#{ARGV[0].match(/(?<=to:)[+]{0,1}\d*/)},#{ARGV[0].match(/(?<=flags:)[^\]]+/)}"
+puts "#{ARGV[0].match(/(?<=from:)[^\]]+/)},#{ARGV[0].match(/(?<=to:)[+]{0,1}[^\]]+/)},#{ARGV[0].match(/(?<=flags:)[^\]]+/)}"
