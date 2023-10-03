bb64c0b7648609d09abaab060070f4f2946b2709
LwaziNcubeX <lwazincubex@gmail.com>
2023-10-04 01:13:27 +0200

    update script to 2 lines

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,12 +1,2 @@
 #!/usr/bin/env ruby
-log = ARGV[0]
-
-from = log.match(/(?<=from:)\w*/)
-
-to = log.match(/(?<=to:)[+]{0,1}\d*/)
-
-flags = log.match(/(?<=flags:)[^\]]+/)
-
-output = "#{from},#{to},#{flags}"
-
-puts output
+puts "#{ARGV[0].match(/(?<=from:)\w*/)},#{ARGV[0].match(/(?<=to:)[+]{0,1}\d*/)},#{ARGV[0].match(/(?<=flags:)[^\]]+/)}"
