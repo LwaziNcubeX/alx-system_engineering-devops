0452fb4a229893f833fea6b6821a42368a54685c
LwaziNcubeX <lwazincubex@gmail.com>
2023-11-25 00:46:36 +0200

    Add Werkzeug pkg

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # A script to install Flask
 package { 'flask':
-  ensure => '2.1.0',
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
