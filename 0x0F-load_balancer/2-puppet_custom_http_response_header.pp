c554b012f0756687b76c3397a1eba57f467e958e
LwaziNcubeX <lwazincubex@gmail.com>
2023-12-05 03:36:12 +0200

    Fix pp issues

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,44 +1,21 @@
-# Add a custom HTTP header with Puppet
-# Install Nginx
+# Custom HTTP header in an Nginx server
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+
+# Install Nginx web server on the server
 package { 'nginx':
-  ensure => 'installed',
+  ensure   => present,
 }
 
-# Allow Nginx through Firewall
-firewall { '100 allow nginx':
-  port   => 80,
-  proto  => 'tcp',
-  action => 'accept',
-}
 
-# Backup default Nginx configuration
-file { '/etc/nginx/sites-available/default.bak':
+file_line { 'add X-Served-By header':
   ensure => 'present',
-  source => '/etc/nginx/sites-available/default',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => "    add_header X-Served-By $hostname;",
 }
 
-# Configure Nginx
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content => "
-    server {
-        listen 80;
-        server_name _;
-
-        location / {
-            add_header X-Served-By $::hostname;
-            add_header Content-Type text/plain;
-            return 200 'Hello World!';
-        }
-    }
-  ",
-  notify  => Service['nginx'],
+exec {'start':
+  command => '/usr/sbin/service nginx start',
 }
-
-# Restart Nginx
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
-}
-
